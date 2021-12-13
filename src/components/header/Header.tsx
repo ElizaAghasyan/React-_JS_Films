b189c9d4c3d468b5d803919d109d1a8668e7b2e4
Eliza_Aghasyan <aghasyaneliza@gmail.com>
2021-12-13 22:10:29 +0400

    React JS Films - Code refactoring

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,24 @@
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Link } from 'react-router-dom';
+
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 const styles = require("./Header.module.scss");
 
 const Header = () => {
     return (
-        <div className={styles.header_content}>
-            <Link to='/'>
-                <h1>Films</h1>
-            </Link>
-            <div>
-                <input
-                    placeholder='Search Films'
-                    onChange={(e) => e.target.value}
-                />
-                <SearchOutlinedIcon style={{fontSize: '2.3em', color: '#fff', margin: '0.4rem'}}/>
+        <header className={styles.header}>
+            <div className={styles.header_content}>
+                <Link to='/'>
+                    <h1>Films</h1>
+                </Link>
+                <div>
+                    <input
+                        placeholder='Search Films'
+                        onChange={(e) => e.target.value}
+                    />
+                    <SearchOutlinedIcon style={{position: 'absolute', transform: 'scale(1.3)', right: '4rem', top: '2rem', color: '#fff'}}/>
+                </div>
             </div>
-        </div>
+        </header>
     );
 }
 
